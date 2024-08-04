1d4b0a75b9a0922fd6ec309160dc689b626730cd
nobkd <44443899+nobkd@users.noreply.github.com>
2024-08-04 23:17:31 +0200

    feat: WIP: make resizable

---
+++ src/splitpane.js
@@ -0,0 +1,43 @@
+// https://phuoc.ng/collection/html-dom/create-resizable-split-views/
+
+export function createHandlers(resizer) {
+    const prev = resizer.previousElementSibling
+    const next = resizer.nextElementSibling
+
+    const elToResize = prev.style.width ? prev : next
+    const factor = elToResize === next ? -1 : 1
+    const parent = resizer.parentNode
+
+    console.log(elToResize)
+
+    const el = document.createElement('div')
+    el.className = 'splitter-overlay'
+
+    let x = 0
+    let width = 0
+
+    function mouseDownHandler($event) {
+        parent.insertAdjacentElement('afterbegin', el)
+
+        x = $event.clientX
+        width = elToResize.getBoundingClientRect().width
+        console.log(width)
+
+        document.addEventListener('mousemove', mouseMoveHandler)
+        document.addEventListener('mouseup', mouseUpHandler)
+    }
+
+    function mouseMoveHandler($event) {
+        const dx = $event.clientX - x
+        // console.log(width, dx, width + dx)
+        elToResize.style.width = `${((width + dx * factor) * 100) / parent.getBoundingClientRect().width}%`
+    }
+
+    function mouseUpHandler() {
+        document.removeEventListener('mousemove', mouseMoveHandler)
+        document.removeEventListener('mouseup', mouseUpHandler)
+        el.remove()
+    }
+
+    return { mouseDownHandler, mouseMoveHandler, mouseUpHandler }
+}
