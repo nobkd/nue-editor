30d8e7504cb26676b14674f9f37a5a999e5218ae
nobkd <44443899+nobkd@users.noreply.github.com>
2024-08-17 04:20:19 +0200

    chore(deps): upgrade to latest nuekit, currently: sometimes broken glow hightlights

--- prepare.js
+++ prepare.js
@@ -6,7 +6,7 @@ const dest = 'src'
 
 const files = {
     'nue-glow/src/glow.js': 'scripts/glow.js',
-    'nue-glow/minified/glow.nano.css': 'style/glow.nano.css',
+    'nue-glow/minified/syntax.nano.css': 'style/syntax.nano.css',
 }
 
 Object.entries(files).forEach(([from, to]) => copyFileSync(join(start, from), join(dest, to)))
